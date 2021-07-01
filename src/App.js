9877086ad3fa633eaa61220c3923f4f6c7eae0a2
vyshakh-97 <vyshakh.a1997@gmail.com>
2021-07-01 17:04:46 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navigation from "./Components/Navigation"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+    <Navigation />
     </div>
   );
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Navigation.css"
+import Leftbar from "./Leftbar"
+const Navigation = () => {
+    return (
+        <div className="nav">
+            <div className="navicon">
+            <Leftbar />
+            </div>
+            <h2>
+            Newsfy
+            </h2>
+            
+        </div>
+    )
+}
+
+export default Navigation
